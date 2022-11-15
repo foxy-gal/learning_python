b873eae6824bc5e602082a5c5194991b00db6b35
foxy-gal <asbezhenar@gmail.com>
2022-11-15 20:45:00 +0300

    change python_base_list.py

--- python_base_list.py
+++ python_base_list.py
@@ -15,19 +15,25 @@ lst = [len(lst[0]), None, len(lst[2]), len(lst[3])]
 print(lst)
 
 #Дан список lst, который содержит не менее элементов. Извлеките из него элементы ,  и  (начиная с ) в обратном порядке.
-list = [1, 23, 00, 514, "cat"]
+some_random_list = [1, 23, 00, 514, "cat"]
 print(list[4:1:-1])
 
 #Добавить в список новый элемент методом append
-list.append(5)
-list.append("breadgit")
-print(list)
-list.append("cat")
+some_random_list.append(5)
+some_random_list.append("breadgit")
+print(some_random_list)
+some_random_list.append("cat")
 
 #удаление элемента из списка
-list.remove(23)
-print(list)
-#list.remove("zombie") #попытка удалить то, чего нет в списке. dslftn jib,re "ValueError: list.remove(x): x not in list"
-print(list)
-list.remove("cat") #удаление элемента, который встречается больше раза в списке
-print(list)
+some_random_list.remove(23)
+print(some_random_list)
+#list.remove("zombie") #попытка удалить то, чего нет в списке. Выдает ошибку "ValueError: list.remove(x): x not in list"
+print(some_random_list)
+some_random_list.remove("cat") #удаление элемента, который встречается больше раза в списке. удалил первый по порядку элемент
+print(some_random_list)
+
+#проверка на наличие элемента в списке
+if 'груши'in some_random_list:
+    some_random_list.remove('груши')
+else:
+    print('Груш в списке покупок нет')
