c7b417916baa218f01aeebc38bcd31a2505e2473
foxy-gal <asbezhenar@gmail.com>
2022-11-19 03:07:14 +0300

    change naming, add base_set.py

---
+++ python/base_set.py
@@ -0,0 +1,12 @@
+#
+python = ['Аня', 'Вася', 'Петя']
+ml = ['Аня']
+maths = ['Катя', 'Женя', 'Вася']
+all_students = python + ml + maths
+print(all_students)
+
+#обращение в множество 0 запись в новую переменную
+#unique_students = set(all_students)
+#print(unique_students)
+
+print(set(all_students))
